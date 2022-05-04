0fc6dea9ebe9a647fe790c5b27c8c5115de363a3
Dmitry Dyabel <orpahdmitry@gmail.com>
2022-05-04 14:05:42 +0300

    new splash screen

---
+++ app/src/main/java/com/mightyhedgehog/doplanner/presentation/main/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.mightyhedgehog.doplanner.presentation.main
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+            val splashScreen = installSplashScreen()
+            splashScreen.setKeepOnScreenCondition { true }
+        }
+        super.onCreate(savedInstanceState)
+        lifecycleScope.launchWhenCreated {
+            delay(3000)
+
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
