288da8ccd43e2edade06980e6bcacff341ec919b
Dmitry Dyabel <orpahdmitry@gmail.com>
2022-05-04 18:40:10 +0300

    Save button on settings screen

--- app/src/main/java/com/mightyhedgehog/doplanner/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/mightyhedgehog/doplanner/presentation/settings/SettingsScreen.kt
@@ -2,13 +2,13 @@ package com.mightyhedgehog.doplanner.presentation.settings
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.mightyhedgehog.doplanner.ui.compoments.settings.ChangeNameTitle
-import com.mightyhedgehog.doplanner.ui.compoments.settings.ChangeStyleTitle
-import com.mightyhedgehog.doplanner.ui.compoments.settings.ColorSchemeBox
+import com.mightyhedgehog.doplanner.ui.compoments.settings.*
 import com.mightyhedgehog.doplanner.ui.theme.*
 import com.mightyhedgehog.doplanner.ui.theme.DoPlannerStyle
 
@@ -22,6 +22,20 @@ fun SettingsScreen(
             .background(DoPlannerTheme.colors.backgroundWhite)
             .fillMaxSize()
     ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            SettingsTitle(modifier = Modifier.padding(start = 16.dp, top = 16.dp, bottom = 8.dp))
+            SettingsSaveClickableTitle(
+                modifier = Modifier.padding(end = 16.dp, top = 16.dp, bottom = 8.dp)
+            )
+        }
+        Divider(
+            modifier = Modifier.padding(start = 8.dp, end = 8.dp),
+            thickness = 0.5.dp
+        )
         ChangeNameTitle(modifier = Modifier.padding(16.dp))
         ChangeStyleTitle(modifier = Modifier.padding(16.dp))
         Row(
