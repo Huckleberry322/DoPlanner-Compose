5dbd57c7e64e4b061b59365025b5e1adbd914c2e
Dmitry Dyabel <orpahdmitry@gmail.com>
2022-05-14 17:36:48 +0300

    Added ConfigData

---
+++ buildSrc/src/main/java/ConfigData.kt
@@ -0,0 +1,10 @@
+object ConfigData {
+
+    const val compileSdkVersion = 31
+    const val minSdkVersion = 26
+    const val targetSdkVersion = 31
+    const val versionCode = 1
+    const val versionName = "1.0"
+
+    const val packageName = "com.mightyhedgehog.colorapp"
+}
